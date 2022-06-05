536b27051ee90815ed917f3e6a2052e6460483fe
Amine BEN ZAGGAGH <amine.benzaggagh@icloud.com>
2022-06-05 21:39:53 +0100

    Done: CI/CD Pipeline Dockerfile Fix

--- src/main/java/com/doctorspolis/backend/configuration/SecurityConfiguration.java
+++ src/main/java/com/doctorspolis/backend/configuration/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.doctorspolis.backend.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+// @Configuration
+// @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
